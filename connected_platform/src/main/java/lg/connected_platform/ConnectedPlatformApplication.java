ed4ef510b5288699fafe1ed049d27126cad86a63
cathy <cathy02473500@gmail.com>
2024-11-02 15:05:17 +0900

    초기 구축

---
+++ connected_platform/src/main/java/lg/connected_platform/ConnectedPlatformApplication.java
@@ -0,0 +1,13 @@
+package lg.connected_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectedPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectedPlatformApplication.class, args);
+	}
+
+}
---
+++ connected_platform/src/test/java/lg/connected_platform/ConnectedPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package lg.connected_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectedPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
