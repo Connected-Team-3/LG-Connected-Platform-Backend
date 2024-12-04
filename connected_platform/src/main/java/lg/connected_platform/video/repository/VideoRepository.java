bcd37c3b43c513d304ca739ac92e1308a00cb001
cathy <cathy02473500@gmail.com>
2024-12-04 20:58:43 +0900

    fix : JPQL 수정

--- connected_platform/src/main/java/lg/connected_platform/video/repository/VideoRepository.java
+++ connected_platform/src/main/java/lg/connected_platform/video/repository/VideoRepository.java
@@ -11,11 +11,11 @@ import java.util.List;
 public interface VideoRepository extends JpaRepository<Video, Long> {
     List<Video> findByCategory(Category category);
 
-    @Query("SELECT v FROM Video v" +
-            "LEFT JOIN v.food f" +
-            "LEFT JOIN f.ingredients i" +
-            "WHERE LOWER(v.title) LIKE LOWER(CONCAT('%', :searchTerm, '%'))" +
-            "OR LOWER(f.name) LIKE LOWER(CONCAT('%', :serachTerm, '%'))" +
+    @Query("SELECT v FROM Video v " +
+            "LEFT JOIN v.food f " +
+            "LEFT JOIN f.ingredients i " +
+            "WHERE LOWER(v.title) LIKE LOWER(CONCAT('%', :searchTerm, '%')) " +
+            "OR LOWER(f.name) LIKE LOWER(CONCAT('%', :searchTerm, '%')) " +
             "OR LOWER(i) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
     List<Video> searchByTitleOrFoodOrIngredients(@Param("searchTerm") String searchTerm);
 }
